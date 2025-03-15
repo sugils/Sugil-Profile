368861fe6177cc3fde3278b4e8edc6d80289e9b3
Sugil <109132770+sugils@users.noreply.github.com>
2025-03-15 22:03:53 +0530

    First Portflio commit andaavaaa

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed= new Typed(".text",
+{
+    strings:["AI/Ml Analyst","Graphic Designer","Full Stack Developer","Python Developer","Gen-AI Developer"],
+typeSpeed:100,
+backSpeed:100,
+baclDelay:1000,
+loop:true
+}
+)
